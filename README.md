75dcbf3bc6b599aaea67e106b8c19d0429f14057
hasendar <mr.alyamkin@gmail.com>
2024-03-12 20:31:33 +0300

    Added Thread factory

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 - npm i bootstrap@5.3.3
 - route:clear
 - route:cache
+- make:factory ThreadFactory -m
+- migrate --seed
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $threads = \App\Models\Thread::factory(20)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
